9efa394dc15f1949ee906763f604eaaa5f5d0c2f
Nils Fischer <nils@designatic.de>
2023-10-28 17:12:07 +0200

    import GlobalStyle

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import GlobalStyle from "styled-components";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
