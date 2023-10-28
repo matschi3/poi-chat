915728b504368300dc378a5ce59648e6d882d1f5
Nils Fischer <nils@designatic.de>
2023-10-28 17:58:37 +0200

    cleared font

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import PoiCard from "@/components/PoiCard";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
