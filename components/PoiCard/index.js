d1d26a5f24d4380890d7fc2952327c3abc8d6cf1
Nils Fischer <nils@designatic.de>
2023-11-02 13:08:35 +0100

    add basic display of data / check

--- components/PoiCard/index.js
+++ components/PoiCard/index.js
@@ -2,8 +2,29 @@ import { StyledPoiCard } from "./PoiCard.styled";
 
 export default function PoiCard({ pois }) {
   return (
-    <StyledPoiCard>
-      <h1>POI-Card</h1>
-    </StyledPoiCard>
+    <>
+      {pois.map((poi) => (
+        <StyledPoiCard key={poi.id}>
+          <h2>{poi.properties.name}</h2>
+          <p>
+            {poi.location.adress.street} {poi.location.adress.housenumber}
+          </p>
+          <ul>
+            Sportarten:
+            {poi.properties.sports.map((sport, index) => {
+              const key = `${index}${sport}`;
+              return <li key={key}>{sport}</li>;
+            })}
+          </ul>
+          <ul>
+            Geräte:
+            {poi.properties.devices.map((device, index) => {
+              const key = `${index}${device}`;
+              return <li key={key}>{device}</li>;
+            })}
+          </ul>
+        </StyledPoiCard>
+      ))}
+    </>
   );
 }
