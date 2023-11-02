c4b2a156a573a854cf2708228a6f295c541b708f
Nils Fischer <nils@designatic.de>
2023-11-02 12:10:57 +0100

    recieve pois prop

--- components/PoiCard/index.js
+++ components/PoiCard/index.js
@@ -1,6 +1,6 @@
 import { StyledPoiCard } from "./PoiCard.styled";
 
-export default function PoiCard() {
+export default function PoiCard({ pois }) {
   return (
     <StyledPoiCard>
       <h1>POI-Card</h1>
