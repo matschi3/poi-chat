7ee780740dfab481224fc4a05dbaea6e9e5bd937
Nils Fischer <nils@designatic.de>
2023-11-12 15:56:52 +0100

    add flexbox to container, add Divider

--- components/PoiBlock/PoiBlock.styled.js
+++ components/PoiBlock/PoiBlock.styled.js
@@ -3,18 +3,29 @@ import styled from "styled-components";
 export const StyledBlockContainer = styled.div`
   position: absolute;
   width: calc(100% - 32px);
-  top: 194px;
+  top: 60px;
   right: 16px;
   left: 16px;
   height: 356px;
   font-size: 14px;
   color: var(--color-black);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const StyledBlockDivider = styled.div`
+  width: calc(100% - 65px);
+  border-top: 1px solid var(--color-gray2);
+  box-sizing: border-box;
+  height: 1px;
 `;
 
 export const StyledBlockBox = styled.div`
   position: relative;
   width: 100%;
-  height: 77px;
+  height: 50px;
   text-align: left;
   font-size: var(--size-mediumsmall);
   color: var(--color-black);
@@ -23,7 +34,7 @@ export const StyledBlockBox = styled.div`
 export const StyledPoiBlock = styled.div`
   position: absolute;
   width: 100%;
-  height: 77px;
+  height: 50px;
   top: 0px;
   right: 0px;
   left: 0px;
@@ -41,23 +52,24 @@ export const StyledBlockImage = styled.div`
 
 export const StyledBlockHeader = styled.div`
   position: absolute;
-  top: 0px;
+  top: 2px;
   left: 66px;
   font-size: 16px;
   font-weight: 600;
+  line-height: 0.9;
 `;
 
 export const StyledBlockText = styled.div`
   position: absolute;
   width: calc(100% - 74px);
-  top: 27px;
+  top: 28px;
   left: 66px;
   display: inline-block;
 `;
 
 export const StyledBlockLastUpdate = styled.div`
   position: absolute;
-  top: 2px;
+  top: 0px;
   right: 0px;
   color: var(--color-gray3);
   text-align: right;
