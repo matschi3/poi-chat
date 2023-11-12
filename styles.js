e2f39359a264540770ff9b7e057647f73b47e6cb
Nils Fischer <nils@designatic.de>
2023-11-12 14:40:20 +0100

    add colors and sizes, room for optimization

--- styles.js
+++ styles.js
@@ -8,8 +8,16 @@ export default createGlobalStyle`
   }
 
   :root {
-    --color-white: #eaeaea;
+    --color-white: #ffffff;
     --color-black: #121212;
+    --color-gray1: #f6f6f6;
+    --color-gray2: #e8e8e8;
+    --color-gray3: #bdbdbd
+    --color-primary: #5db075;
+
+    --size-mediumsmall: 14px;
+    --size-medium: 16px;
+    --size-large: 30px;
   }
 
   body {
