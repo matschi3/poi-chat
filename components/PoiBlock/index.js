6ed746b4c08b71da33c9b3e4d1d24ba07bc7d7a8
Nils Fischer <nils@designatic.de>
2023-11-12 15:57:34 +0100

    add slice for text>32, add divider

--- components/PoiBlock/index.js
+++ components/PoiBlock/index.js
@@ -1,5 +1,6 @@
 import {
   StyledPoiBlock,
+  StyledBlockDivider,
   StyledBlockBox,
   StyledBlockImage,
   StyledBlockHeader,
@@ -9,13 +10,24 @@ import {
 
 export default function PoiBlock({ poi }) {
   return (
-    <StyledBlockBox>
-      <StyledPoiBlock>
-        <StyledBlockImage />
-        <StyledBlockHeader>{poi.properties.name}</StyledBlockHeader>
-        <StyledBlockText>{poi.properties.description}</StyledBlockText>
-        <StyledBlockLastUpdate>6m ago</StyledBlockLastUpdate>
-      </StyledPoiBlock>
-    </StyledBlockBox>
+    <>
+      <StyledBlockBox>
+        <StyledPoiBlock>
+          <StyledBlockImage />
+          <StyledBlockHeader>
+            {poi.properties.name.length < 32
+              ? poi.properties.name
+              : poi.properties.name.slice(0, 32) + ".."}
+          </StyledBlockHeader>
+          <StyledBlockText>
+            {poi.properties.description.length < 32
+              ? poi.properties.description
+              : poi.properties.description.slice(0, 32) + ".."}
+          </StyledBlockText>
+          {/* <StyledBlockLastUpdate>6m ago</StyledBlockLastUpdate> */}
+        </StyledPoiBlock>
+      </StyledBlockBox>
+      <StyledBlockDivider />
+    </>
   );
 }
