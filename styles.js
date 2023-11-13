3ff752daaf18c9a52ecbee181e469bc05b31962b
Nils Fischer <nils@designatic.de>
2023-11-13 15:56:21 +0100

    edit

--- styles.js
+++ styles.js
@@ -17,7 +17,7 @@ export default createGlobalStyle`
     --color-black: #121212;
     --color-gray1: #f6f6f6;
     --color-gray2: #e8e8e8;
-    --color-gray3: #bdbdbd
+    --color-gray3: #bdbdbd;
     --color-primary: #5db075;
 
     --size-mediumsmall: 14px;
