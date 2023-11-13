9e55cce1a705d3f3596fdbf589d181db6f449337
Nils Fischer <nils@designatic.de>
2023-11-13 14:07:51 +0100

    add basic styled elements

--- components/PoiCard/PoiCard.styled.js
+++ components/PoiCard/PoiCard.styled.js
@@ -1,5 +1,44 @@
 import styled from "styled-components";
 
 export const StyledPoiCard = styled.div`
+  position: relative;
+  top: 60px;
+  width: 100%;
   border: solid 1px black;
+`;
+
+export const StyledCardImage = styled.div`
+  position: absolute;
+  top: 10px;
+  left: calc(50% - 75px);
+  width: 150px;
+  height: 150px;
+  border-radius: 55px;
+  background-color: var(--color-gray2);
+`;
+
+export const StyledCardHeading = styled.h2`
+  position: absolute;
+  top: 125px;
+`;
+
+export const StyledCardDescription = styled.p`
+  position: absolute;
+  top: 160px;
+`;
+
+export const StyledCardAdress = styled.p`
+  position: absolute;
+  top: 200px;
+`;
+
+export const StyledCardListContainer = styled.div`
+  position: absolute;
+  top: 240px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledCardUl = styled.ul`
+  list-style-type: none;
 `;
