b0e277f2116991cc120fa59128a27295532c179c
Nils Fischer <nils@designatic.de>
2023-11-13 15:15:35 +0100

    add li + liDot + flex to ul

--- components/PoiCard/PoiCard.styled.js
+++ components/PoiCard/PoiCard.styled.js
@@ -35,10 +35,36 @@ export const StyledCardAdress = styled.p`
 export const StyledCardListContainer = styled.div`
   position: absolute;
   top: 240px;
+  left: 16px;
+  width: calc(100% - 32px);
   display: flex;
   flex-direction: column;
+  text-align: left;
 `;
 
 export const StyledCardUl = styled.ul`
   list-style-type: none;
+  width: 100%;
+  font-size: var(--size-large);
+  font-weight: 500;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 0;
+`;
+
+export const StyledCardLi = styled.li`
+  font-size: var(--size-mediumsmall);
+  position: relative;
+  margin-left: 32px;
+`;
+
+export const StyledCardLiDot = styled.span`
+  position: absolute;
+  top: 0;
+  left: -28px;
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  background-color: #5db075;
 `;
