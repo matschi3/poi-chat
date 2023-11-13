5d77acd4ff470325bddc8583ffad9bb0640c70bf
Nils Fischer <nils@designatic.de>
2023-11-13 14:08:02 +0100

    add basic styled elements

--- components/PoiCard/index.js
+++ components/PoiCard/index.js
@@ -1,26 +1,40 @@
-import { StyledPoiCard } from "./PoiCard.styled";
+import {
+  StyledPoiCard,
+  StyledCardImage,
+  StyledCardHeading,
+  StyledCardDescription,
+  StyledCardAdress,
+  StyledCardListContainer,
+  StyledCardUl,
+} from "./PoiCard.styled";
 
 export default function PoiCard({ poi }) {
   return (
     <StyledPoiCard>
-      <h2>{poi.properties.name}</h2>
-      <p>
+      <StyledCardImage />
+      <StyledCardHeading>{poi.properties.name}</StyledCardHeading>
+      <StyledCardDescription>
+        {poi.properties.description}
+      </StyledCardDescription>
+      <StyledCardAdress>
         {poi.location.adress.street} {poi.location.adress.housenumber}
-      </p>
-      <ul>
-        Sportarten:
-        {poi.properties.sports.map((sport, index) => {
-          const key = `${index}${sport}`;
-          return <li key={key}>{sport}</li>;
-        })}
-      </ul>
-      <ul>
-        Geräte:
-        {poi.properties.devices.map((device, index) => {
-          const key = `${index}${device}`;
-          return <li key={key}>{device}</li>;
-        })}
-      </ul>
+      </StyledCardAdress>
+      <StyledCardListContainer>
+        <StyledCardUl>
+          Sportarten:
+          {poi.properties.sports.map((sport, index) => {
+            const key = `${index}${sport}`;
+            return <li key={key}>{sport}</li>;
+          })}
+        </StyledCardUl>
+        <ul>
+          Geräte:
+          {poi.properties.devices.map((device, index) => {
+            const key = `${index}${device}`;
+            return <li key={key}>{device}</li>;
+          })}
+        </ul>
+      </StyledCardListContainer>
     </StyledPoiCard>
   );
 }
