4dcc55eff1030b2d7a8d600d5b6fd820e77b7962
Nils Fischer <nils@designatic.de>
2023-11-25 10:34:35 +0100

    complete Schema/dbModel

--- db/models/Asset.js
+++ db/models/Asset.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 
 const assetSchema = new Schema({
   uuid: { type: String, required: true },
+  mainImage: { type: String, required: false },
+  images: { type: [Array], required: false },
 });
 
 const Asset = mongoose.models.Asset || mongoose.model("Asset", assetSchema);
