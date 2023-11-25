079745481ac44193b26d3684e0e9db4a16d36033
Nils Fischer <nils@designatic.de>
2023-11-25 10:24:22 +0100

    complete Schema/dbModel

--- db/models/Location.js
+++ db/models/Location.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema;
 
 const locationSchema = new Schema({
   uuid: { type: String, required: true },
+  geometry: { type: [Schema.Types.ObjectId], ref: "Geometry" },
+  adress: { type: [Schema.Types.ObjectId], ref: "Adress" },
+  hint: { type: String, required: false },
 });
 
 const Location =
