5d4245f37656aae1d6b02009e2528f1c3ed13664
Nils Fischer <nils@designatic.de>
2023-11-25 10:36:18 +0100

    complete Schema/dbModel

--- db/models/Event.js
+++ db/models/Event.js
@@ -4,6 +4,11 @@ const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
   uuid: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  dateStart: { type: Date, required: true }, // maybe change to string later
+  dateEnd: { type: Date, required: true }, // maybe change to string later
+  dateCreated: { type: Date, required: true }, // maybe change to string later
 });
 
 const Event = mongoose.models.Event || mongoose.model("Event", eventSchema);
