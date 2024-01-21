0ee9985e97043e8158994307355ea2016c9d5261
Nils Fischer <nils@designatic.de>
2024-01-21 12:50:46 +0100

    import Styled Form elements

--- components/PoiForm/index.js
+++ components/PoiForm/index.js
@@ -1,3 +1,5 @@
+import { FormContainer, Label, Input } from "./PoiForm.styled";
+
 export default function PoiForm({ onSubmit, formName }) {
   function handleSubmit(event) {
     event.preventDefault();
