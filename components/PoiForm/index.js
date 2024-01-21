4a888bea0923324a6e71391fb4ceed2f9bf11559
Nils Fischer <nils@designatic.de>
2024-01-21 12:42:10 +0100

    init handleSubmit function with empty newPoi-Object

--- components/PoiForm/index.js
+++ components/PoiForm/index.js
@@ -1,3 +1,12 @@
 export default function PoiForm({ onSubmit, formName }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    const newPoi = {};
+
+    onSubmit(newPoi);
+  }
   return <></>;
 }
