0e66c015b929ff73b3b1ab24da9fd76d65ad8543
Nils Fischer <nils@designatic.de>
2024-01-21 12:32:58 +0100

    create basic createPoi function with POST operation

--- pages/poi/create.js
+++ pages/poi/create.js
@@ -1,3 +1,14 @@
 export default function CreatePoiPage() {
+  async function createPoi(newPoi) {
+    const response = await fetch("api/pois", {
+      method: "POST",
+      headers: { "Content-Type": "aplication/json" },
+      body: JSON.stringify(newPoi),
+    });
+    if (response.ok) {
+      console.log("Poi created successfully");
+    }
+  }
+
   return <></>;
 }
