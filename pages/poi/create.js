1e765c5177c2e554d4dcaaaba874d2272b7f80c4
Nils Fischer <nils@designatic.de>
2024-01-24 12:50:44 +0100

    fix relative path

--- pages/poi/create.js
+++ pages/poi/create.js
@@ -3,7 +3,7 @@ import PoiForm from "@/components/PoiForm/index.js";
 export default function CreatePoiPage() {
   async function createPoi(newPoi) {
     console.log(newPoi);
-    const response = await fetch("../api/pois", {
+    const response = await fetch("/api/pois", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newPoi),
