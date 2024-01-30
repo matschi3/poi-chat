0ba3a8d37287948c7e45b94e14bdb8818a271678
Nils Fischer <nils@designatic.de>
2024-01-30 15:10:56 +0100

    init locations api route

---
+++ pages/api/locations/index.js
@@ -0,0 +1,28 @@
+import dbConnect from "../../../db/connect.js";
+import Location from "../../../db/models/Location.js";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const locations = await Location.find({})
+      .populate({
+        path: "geometry",
+        populate: { path: "coordinates" },
+      })
+      .populate("adress");
+    if (!locations) {
+      return response.status(404).json({ message: "No locations found" });
+    }
+    return response.status(200).json(locations);
+  }
+  if (request.method === "POST") {
+    try {
+      const newLocation = request.body;
+      await Location.create(newLocation);
+      return response.status(201).json({ status: "Location created." });
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
+  }
+  return response.status(405).json({ status: "Method not allowed" });
+}
