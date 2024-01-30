dda0fe96199fea73e29438d13a6022e04c0a7365
Nils Fischer <nils@designatic.de>
2024-01-30 15:51:16 +0100

    init events api route

---
+++ pages/api/events/index.js
@@ -0,0 +1,23 @@
+import dbConnect from "../../../db/connect.js";
+import Event from "../../../db/models/Event.js";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const events = await Event.find({});
+    if (!events) {
+      return response.status(404).json({ message: "No events found" });
+    }
+    return response.status(200).json(events);
+  }
+  if (request.method === "POST") {
+    try {
+      const newEvent = request.body;
+      await Event.create(newEvent);
+      return response.status(201).json({ status: "Event created." });
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
+  }
+  return response.status(405).json({ status: "Method not allowed" });
+}
