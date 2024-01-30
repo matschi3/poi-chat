fa6af9730777291a1be9e6987124d60776d90d76
Nils Fischer <nils@designatic.de>
2024-01-30 14:29:02 +0100

    init activities api route

---
+++ pages/api/activities/index.js
@@ -0,0 +1,31 @@
+import dbConnect from "../../../db/connect.js";
+import Activity from "../../../db/models/Activity.js";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const activities = await Activity.find({})
+      .populate({
+        path: "devices",
+        populate: [{ path: "assets", populate: { path: "images" } }],
+      })
+      .populate({
+        path: "sports",
+        populate: [{ path: "assets", populate: { path: "images" } }],
+      });
+    if (!activities) {
+      return response.status(404).json({ message: "No activities found" });
+    }
+    return response.status(200).json(activities);
+  }
+  if (request.method === "POST") {
+    try {
+      const newActivity = request.body;
+      await Activity.create(newActivity);
+      return response.status(201).json({ status: "Activity created." });
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
+  }
+  return response.status(405).json({ status: "Method not allowed" });
+}
