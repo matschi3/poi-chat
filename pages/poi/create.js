8b4ede368e39e43a327af5f861e6a4d70bd69b96
Nils Fischer <nils@designatic.de>
2024-01-31 20:48:13 +0100

    delete previous made POST+GET and simplify at backend api/route POST method of poi

--- pages/poi/create.js
+++ pages/poi/create.js
@@ -1,50 +1,11 @@
 import PoiForm from "@/components/PoiForm/index.js";
-import { useState } from "react";
 
 export default function CreatePoiPage() {
-  const [geometry, setGeometry] = useState();
-
-  async function createGeometry(geometry) {
-    const response = await fetch("/api/geometries", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(geometry),
-    });
-    if (response.ok) {
-      console.log("Geometry created successfully");
-    }
-  }
-  async function fetchGeometryId(uuid) {
-    const response = await fetch("/api/geometries", {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response.ok) {
-      const geometryData = await response.json();
-      const foundGeometry = geometryData.find((geometryObject) => {
-        return geometryObject.uuid === uuid;
-      });
-      if (foundGeometry) {
-        setGeometry(foundGeometry._id);
-      }
-    }
-  }
-
   async function createPoi(newPoi) {
-    console.log("newPoi", newPoi);
-    /* geometry */
-    if (newPoi.location[0].geometry[0] === "n/a") {
-      setGeometry([]);
-    } else {
-      const newPoiGeometry = newPoi.location[0].geometry[0];
-      await createGeometry(newPoiGeometry);
-      await fetchGeometryId(newPoiGeometry.uuid);
-    }
-    /* POST newPoiToDb */
     const response = await fetch("/api/pois", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newPoiToDb),
+      body: JSON.stringify(newPoi),
     });
     if (response.ok) {
       console.log("Poi created successfully");
