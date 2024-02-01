959c4083f8488b746ad8aac51120a8a9f43fd39e
Nils Fischer <nils@designatic.de>
2024-02-02 00:52:47 +0100

    import StyledHeader

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,15 +1,19 @@
 import Head from "next/head";
+import { StyledHeader } from "./Header.styled";
 
 export default function Header() {
   return (
-    <Head>
-      <title>POI-Chat</title>
-      <meta
-        name="description"
-        content="Chat with like-minded an get connected right at your Point of Interest"
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <>
+      <Head>
+        <title>POI-Chat</title>
+        <meta
+          name="description"
+          content="Chat with like-minded an get connected right at your Point of Interest"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <StyledHeader></StyledHeader>
+    </>
   );
 }
