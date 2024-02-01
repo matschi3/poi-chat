1dcbda98008fccd355b6b2ff16bc2cdf33d35e0d
Nils Fischer <nils@designatic.de>
2024-02-02 00:46:16 +0100

    add styling

--- components/Header/Header.styled.js
+++ components/Header/Header.styled.js
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const StyledHeader = styled.header``;
+export const StyledHeader = styled.header`
+  background-color: var(--color-primary);
+  width: 100%;
+  height: 60px;
+`;
