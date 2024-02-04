869d3ad38560a3cb98ceefa39066fabf3b83d777
Nils Fischer <nils@designatic.de>
2024-02-04 23:07:28 +0100

    add flexbox, add HeaderTitle

--- components/Header/Header.styled.js
+++ components/Header/Header.styled.js
@@ -4,4 +4,14 @@ export const StyledHeader = styled.header`
   background-color: var(--color-primary);
   width: 100%;
   height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.h1`
+  color: var(--color-white);
+  font-size: 30px;
+  font-weight: 600;
+  text-shadow: 2px 2px 2px var(--color-black);
 `;
