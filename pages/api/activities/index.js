1854403a0b4faf36bccccd275fb0b148132d5847
Nils Fischer <nils@designatic.de>
2024-02-05 15:22:44 +0100

    add DELETE operation

--- pages/api/activities/index.js
+++ pages/api/activities/index.js
@@ -26,6 +26,12 @@ export default async function handler(request, response) {
     } catch (error) {
       response.status(400).json({ error: error.message });
     }
+  } else if (request.method === "DELETE") {
+    const deletedActivity = await Activity.findByIdAndDelete(id);
+    if (!deletedActivity) {
+      return response.status(404).json({ message: "No Activity found" });
+    }
+    return response.status(200).json(deletedActivity);
   }
   return response.status(405).json({ status: "Method not allowed" });
 }
