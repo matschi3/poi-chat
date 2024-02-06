2c0b181a598681fc747f82b9686d1a2b83e2ebd7
Nils Fischer <nils@designatic.de>
2024-02-06 13:09:11 +0100

    init api/activities/[id] route and move DELETE here from api/activities

--- pages/api/activities/[id].js
+++ pages/api/activities/[id].js
@@ -2,9 +2,10 @@ import dbConnect from "../../../db/connect.js";
 import Activity from "../../../db/models/Activity.js";
 
 export default async function handler(request, response) {
+  const { id } = request.query;
   await dbConnect();
   if (request.method === "GET") {
-    const activities = await Activity.find({})
+    const activity = await Activity.findById(id)
       .populate({
         path: "devices",
         populate: [{ path: "assets", populate: { path: "images" } }],
@@ -13,19 +14,16 @@ export default async function handler(request, response) {
         path: "sports",
         populate: [{ path: "assets", populate: { path: "images" } }],
       });
-    if (!activities) {
-      return response.status(404).json({ message: "No activities found" });
+    if (!activity) {
+      return response.status(404).json({ message: "No activity found" });
     }
-    return response.status(200).json(activities);
-  }
-  if (request.method === "POST") {
-    try {
-      const newActivity = request.body;
-      await Activity.create(newActivity);
-      return response.status(201).json({ status: "Activity created." });
-    } catch (error) {
-      response.status(400).json({ error: error.message });
+    return response.status(200).json(activity);
+  } else if (request.method === "DELETE") {
+    const deletedActivity = await Activity.findByIdAndDelete(id);
+    if (!deletedActivity) {
+      return response.status(404).json({ message: "No Activity found" });
     }
+    return response.status(200).json(deletedActivity);
   }
   return response.status(405).json({ status: "Method not allowed" });
 }
