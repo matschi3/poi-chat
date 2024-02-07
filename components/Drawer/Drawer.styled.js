67faa61892ad81824473cb022a484614dc6ab649
Nils Fischer <nils@designatic.de>
2024-02-07 19:12:37 +0100

    init Drawer

---
+++ components/Drawer/Drawer.styled.js
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const StyledDrawer = styled.div``;
---
+++ components/Drawer/index.js
@@ -0,0 +1,5 @@
+import { StyledDrawer } from "./Drawer.styled";
+
+export default function Drawer() {
+  return <StyledDrawer></StyledDrawer>;
+}
