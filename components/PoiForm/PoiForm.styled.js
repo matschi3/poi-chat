2b038ba01a470580911fc07dc626308c5ed18df9
Nils Fischer <nils@designatic.de>
2024-02-07 21:17:22 +0100

    init PageFormContainer to center and eliminate Headers top space

--- components/PoiForm/PoiForm.styled.js
+++ components/PoiForm/PoiForm.styled.js
@@ -1,8 +1,18 @@
 import styled from "styled-components";
 
+export const FormPageContainer = styled.section`
+  position: absolute;
+  top: var(--header-height);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
 export const FormContainer = styled.form`
+  position: absolute;
   display: flex;
   flex-direction: column;
+  gap: 1em;
 `;
 
 export const Fieldset = styled.fieldset`
