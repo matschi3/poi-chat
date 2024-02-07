75c956ed0325e1e34274955287f110c11050d58c
Nils Fischer <nils@designatic.de>
2024-02-07 17:07:47 +0100

    add SearchClearButton

--- components/Search/Search.styled.js
+++ components/Search/Search.styled.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const StyledSearchContainer = styled.section`
-  position: absolute;
+  position: relative;
   top: var(--header-height);
   height: 50px;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: var(--color-gray3);
+  background-color: var(--color-gray2);
 `;
 
 export const SearchInput = styled.input`
@@ -16,4 +16,19 @@ export const SearchInput = styled.input`
   height: 32px;
   border-radius: 16px;
   caret-color: var(--color-black);
+`;
+
+export const SearchClearButton = styled.span`
+  position: absolute;
+  background-color: var(--color-gray3);
+  &:hover {
+    background-color: var(--color-warning);
+  }
+  right: 22px;
+  top: 14px;
+  width: 22px;
+  height: 22px;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 22px;
 `;
