4d876f08266c1d1ccff3a6e32838ca55533fcd61
Nils Fischer <nils@designatic.de>
2024-02-07 20:25:00 +0100

    add Drawers Container and style both

--- components/Drawer/Drawer.styled.js
+++ components/Drawer/Drawer.styled.js
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 
-export const StyledDrawer = styled.div``;
+export const DrawerContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 140px;
+  width: 100vw;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+`;
+
+export const StyledDrawer = styled.div`
+  height: 140px;
+  width: min(80%, 300px);
+  background-color: var(--color-gray2);
+  border-radius: 16px 16px 0 0;
+`;
