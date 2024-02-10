5ac17e748f1e80b1933766cf04d90e3916fcd4b5
Nils Fischer <nils@designatic.de>
2024-02-10 21:15:28 +0100

    add SessionProvider to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import GlobalStyle from "../styles.js";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <>
+    <SessionProvider session={session}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
