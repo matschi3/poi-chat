b7eb5e260d1377a2ac9d458669a8f533116421c1
Nils Fischer <nils@designatic.de>
2024-02-12 01:44:10 +0100

    add Discord as Auth Provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,5 +7,9 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_ID,
+      clientSecret: process.env.DISCORD_SECRET,
+    }),
   ],
 });
