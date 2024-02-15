e1edb851cbf66e26af63e512dfcf0a8cb4f824a1
Nils Fischer <nils@designatic.de>
2024-02-15 12:54:07 +0100

    add width

--- components/Footer/Footer.styled.js
+++ components/Footer/Footer.styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledFooter = styled.footer`
   position: fixed;
   bottom: 0;
+  width: 100%;
   height: var(--header-height);
   background-color: var(--color-gray3);
 `;
