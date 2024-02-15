fd1a86cee2170989e93bc85ee9cc20e5e904d5c9
Nils Fischer <nils@designatic.de>
2024-02-15 12:21:08 +0100

    init base Footer

---
+++ components/Footer/Footer.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  position: fixed;
+  bottom: 0;
+  height: var(--header-height);
+  background-color: var(--color-gray3);
+`;
---
+++ components/Footer/index.js
@@ -0,0 +1,5 @@
+import { StyledFooter } from "./Footer.styled";
+
+export default function Footer() {
+  return <StyledFooter />;
+}
