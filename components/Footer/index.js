05653629582a85f2deb60214776cac6f00311a3f
Nils Fischer <nils@designatic.de>
2024-02-18 19:46:00 +0100

    add StyledNavLogin + import and use useSessions status and session to decide which Footer to display

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,16 +2,31 @@ import {
   StyledFooter,
   StyledPlaceholder,
   StyledNavProfile,
+  StyledNavLogin,
 } from "./Footer.styled";
+import { useSession } from "next-auth/react";
 
 export default function Footer() {
-  return (
-    <StyledFooter>
-      <StyledPlaceholder />
-      <StyledPlaceholder />
-      <StyledPlaceholder />
-      <StyledPlaceholder />
-      <StyledNavProfile />
-    </StyledFooter>
-  );
+  const { data: session, status } = useSession();
+  if (status === "loading") {
+    return null;
+  }
+
+  if (session) {
+    return (
+      <StyledFooter>
+        <StyledPlaceholder />
+        <StyledPlaceholder />
+        <StyledPlaceholder />
+        <StyledPlaceholder />
+        <StyledNavProfile />
+      </StyledFooter>
+    );
+  } else {
+    return (
+      <StyledFooter>
+        <StyledNavLogin />
+      </StyledFooter>
+    );
+  }
 }
