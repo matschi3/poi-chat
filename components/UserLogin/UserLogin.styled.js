baf57efb40381d3d51912d679d51b16c75da3679
Nils Fischer <nils@designatic.de>
2024-02-20 19:38:05 +0100

    init basic UserLogin

---
+++ components/UserLogin/UserLogin.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const StyledLoginGridPage = styled.div`
+  display: grid;
+  height: 100vh;
+  width: 100vw;
+  place-items: center;
+  background-color: var(--color-gray4);
+`;
+
+export const StyledLogin = styled.div`
+  text-align: center;
+  margin: 1rem 0;
+  width: 343px;
+`;
+
+export const StyledLoginCard = styled.div`
+  margin: 2rem 0;
+  background-color: var(--color-gray5);
+  border-radius: 2rem;
+  padding: 1.25rem 2rem;
+`;
