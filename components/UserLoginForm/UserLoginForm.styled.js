721ab56535515394ee27e35fbb928316c409fddb
Nils Fischer <nils@designatic.de>
2024-02-21 13:07:37 +0100

    add exports for styled-components

--- components/UserLoginForm/UserLoginForm.styled.js
+++ components/UserLoginForm/UserLoginForm.styled.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
-const StyledForm = styled.form`
+export const StyledForm = styled.form`
   display: block;
 `;
 
-const StyledInputContainer = styled.div`
+export const StyledInputContainer = styled.div`
   margin-bottom: 1rem;
 `;
 
-const StyledLabel = styled.label`
+export const StyledLabel = styled.label`
   color: var(--color-gray2);
   font-weight: 500;
   margin-bottom: 0.25rem;
@@ -16,7 +16,7 @@ const StyledLabel = styled.label`
   text-align: left;
 `;
 
-const StyledInput = styled.input`
+export const StyledInput = styled.input`
   border-radius: 0.5rem;
   font-size: 1rem;
   padding: 0.5rem 1rem;
