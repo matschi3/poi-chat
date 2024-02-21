41554ae180f2e80200d8a5decdede0c6b2703b3d
Nils Fischer <nils@designatic.de>
2024-02-21 21:12:28 +0100

    rewrite api route to fit existing style

--- pages/api/register/index.js
+++ pages/api/register/index.js
@@ -1,26 +1,23 @@
 import User from "@/db/models/User";
 import dbConnect from "@/db/connect";
 import bcrypt from "bcryptjs";
-import { NextResponse } from "next/server";
 
-export const POST = async (request) => {
-  const { email, password } = await request.json();
-
+export default async function handler(request, response) {
   await dbConnect();
-
-  const existingUser = await User.findOne({ email });
-
-  if (existingUser) {
-    return new NextResponse("Email is already in use", { status: 400 });
+  if (request.method === "POST") {
+    try {
+      const { email, password } = request.body;
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return response.status(400).json({ status: "Email is already in use" });
+      }
+      const hashedPassword = await bcrypt.hash(password, 5);
+      const newUser = new User({ email, password: hashedPassword });
+      await User.create(newUser);
+      return response.status(201).json({ status: "User is created" });
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
   }
-
-  const hashedPassword = await bcrypt.hash(password, 5);
-  const newUser = new User({ email, password: hashedPassword });
-
-  try {
-    await newUser.save();
-    return new NextResponse("User is created", { status: 200 });
-  } catch (error) {
-    return NextResponse.error(error, { status: 500 });
-  }
-};
+  return response.status(405).json({ status: "Method not allowed" });
+}
