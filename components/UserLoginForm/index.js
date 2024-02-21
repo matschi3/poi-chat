b5b763bdf577d3166362ac38a2332c6c611ce7c7
Nils Fischer <nils@designatic.de>
2024-02-21 13:51:04 +0100

    add purpose to render conditianal to reuse component for register as well and call different function

--- components/UserLoginForm/index.js
+++ components/UserLoginForm/index.js
@@ -7,7 +7,7 @@ import {
 import { StyledProviderButton } from "../UserLogin/UserLogin.styled";
 import { signIn } from "next-auth/react";
 
-export default function UserLoginForm() {
+export default function UserLoginForm({ purpose }) {
   const handleSignIn = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -17,29 +17,60 @@ export default function UserLoginForm() {
     console.log(data.password);
     /* signIn("credentials", { callbackUrl: "/" }); */
   };
-  return (
-    <StyledForm onSubmit={handleSignIn}>
-      <StyledInputContainer>
-        <StyledLabel htmlFor="email">Email</StyledLabel>
-        <StyledInput
-          type="email"
-          id="email"
-          name="email"
-          placeholder="mail@provider.com"
-        />
-      </StyledInputContainer>
-      <StyledInputContainer>
-        <StyledLabel htmlFor="password">Password</StyledLabel>
-        <StyledInput type="password" id="password" name="password" />
-      </StyledInputContainer>
-      <StyledProviderButton
-        type="submit"
-        provider="Credentials"
-        $backcolor="var(--color-gray3)"
-        $textcolor="var(--color-black)"
-      >
-        <span>Sign in with Email & Password</span>
-      </StyledProviderButton>
-    </StyledForm>
-  );
+  const handleRegister = async (event) => {};
+  if (purpose === "login") {
+    return (
+      <StyledForm onSubmit={handleSignIn}>
+        <StyledInputContainer>
+          <StyledLabel htmlFor="email">Email</StyledLabel>
+          <StyledInput
+            type="email"
+            id="email"
+            name="email"
+            placeholder="mail@provider.com"
+          />
+        </StyledInputContainer>
+        <StyledInputContainer>
+          <StyledLabel htmlFor="password">Password</StyledLabel>
+          <StyledInput type="password" id="password" name="password" />
+        </StyledInputContainer>
+        <StyledProviderButton
+          type="submit"
+          provider="Credentials"
+          $backcolor="var(--color-gray3)"
+          $textcolor="var(--color-black)"
+        >
+          <span>Sign in with Email & Password</span>
+        </StyledProviderButton>
+      </StyledForm>
+    );
+  } else if (purpose === "register") {
+    return (
+      <StyledForm onSubmit={handleRegister}>
+        <StyledInputContainer>
+          <StyledLabel htmlFor="email">Email</StyledLabel>
+          <StyledInput
+            type="email"
+            id="email"
+            name="email"
+            placeholder="mail@provider.com"
+          />
+        </StyledInputContainer>
+        <StyledInputContainer>
+          <StyledLabel htmlFor="password">Password</StyledLabel>
+          <StyledInput type="password" id="password" name="password" />
+        </StyledInputContainer>
+        <StyledProviderButton
+          type="submit"
+          provider="Credentials"
+          $backcolor="var(--color-gray3)"
+          $textcolor="var(--color-black)"
+        >
+          <span>Register new with Email & Password</span>
+        </StyledProviderButton>
+      </StyledForm>
+    );
+  } else {
+    return <p>missing purpose @ UserLoginForm</p>;
+  }
 }
