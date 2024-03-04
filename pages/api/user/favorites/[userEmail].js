1e6a1f08719b2138b1c818e0ffc2b0dd32a9533a
Nils Fischer <nils@designatic.de>
2024-03-04 20:21:35 +0100

    add PUT and DELETE operation to add and delete a users favorite Poi to user.favorites

--- pages/api/user/favorites/[userEmail].js
+++ pages/api/user/favorites/[userEmail].js
@@ -15,6 +15,36 @@ export default async function handler(request, response) {
     } catch (error) {
       return response.status(500).json({ message: "Server error" });
     }
+  } else if (request.method === "PUT") {
+    try {
+      const user = await User.findOne({ email: userEmail });
+      if (user) {
+        const updatedFavorites = [...user.favorites, request.body.poiId];
+        user.favorites = updatedFavorites;
+        await user.save();
+        return response.status(200).json({ favorites: user.favorites });
+      } else {
+        return response.status(404).json({ message: "No user found" });
+      }
+    } catch (error) {
+      return response.status(500).json({ message: "Server error" });
+    }
+  } else if (request.method === "DELETE") {
+    try {
+      const user = await User.findOne({ email: userEmail });
+      if (user) {
+        const updatedFavorites = user.favorites.filter((fav) => {
+          return fav.toString() !== request.body.poiId.toString();
+        });
+        user.favorites = updatedFavorites;
+        await user.save();
+        return response.status(200).json({ favorites: user.favorites });
+      } else {
+        return response.status(404).json({ message: "No user found" });
+      }
+    } catch (error) {
+      return response.status(500).json({ message: "Server error" });
+    }
   }
   return response.status(405).json({ status: "Method not allowed" });
 }
