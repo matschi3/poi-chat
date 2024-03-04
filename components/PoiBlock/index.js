30cb087e48f853881ab4852e7d1064de1e8b7824
Nils Fischer <nils@designatic.de>
2024-03-04 20:23:46 +0100

    add toggleFavorite function into PoiBlock component to toggle the users favorite Pois with both PUT + DELETE method to set on users database entry+ basic toggleButton

--- components/PoiBlock/index.js
+++ components/PoiBlock/index.js
@@ -8,9 +8,48 @@ import {
   StyledBlockLastUpdate,
 } from "./PoiBlock.styled";
 import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 export default function PoiBlock({ poi }) {
   const router = useRouter();
+  const { data: session } = useSession();
+
+  async function toggleFavorite(id) {
+    try {
+      const response = await fetch(
+        `/api/user/favorites/${session.user.email}`,
+        {
+          method: "GET",
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Server responded with status: ${response.status}`);
+      }
+      const favs = await response.json();
+      let method = favs.favorites.includes(id) ? "DELETE" : "PUT";
+
+      const updateResponse = await fetch(
+        `/api/user/favorites/${session.user.email}`,
+        {
+          method: method,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ poiId: id }),
+        }
+      );
+      if (method === "DELETE" && router.replace("/bookmarks"));
+      if (!updateResponse.ok) {
+        throw new Error(
+          `Failed to update favorites with status: ${updateResponse.status}`
+        );
+      }
+      const updatedFavs = await updateResponse.json();
+    } catch (error) {
+      return error;
+    }
+  }
+
   return (
     <>
       <StyledBlockBox
@@ -33,6 +72,7 @@ export default function PoiBlock({ poi }) {
           {/* <StyledBlockLastUpdate>6m ago</StyledBlockLastUpdate> */}
         </StyledPoiBlock>
       </StyledBlockBox>
+      <button onClick={() => toggleFavorite(poi._id)}>Favorite</button>
       <StyledBlockDivider />
     </>
   );
