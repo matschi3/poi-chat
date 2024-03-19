6eba3f69945ed58a2fdce468409eb63eebd75fa9
Nils Fischer <nils@designatic.de>
2024-03-19 18:30:29 +0100

    add jsonWebToken check to protect api route from non-logged in users

--- pages/api/user/role/[userEmail].js
+++ pages/api/user/role/[userEmail].js
@@ -1,21 +1,30 @@
 import dbConnect from "@/db/connect";
 import User from "@/db/models/User";
+import { getToken } from "next-auth/jwt";
 
 /* to get users ROLE */
 export default async function handler(request, response) {
-  await dbConnect();
-  const { userEmail } = request.query;
-  if (request.method === "GET") {
-    try {
-      const user = await User.findOne({ email: userEmail });
-      if (user) {
-        return response.status(200).json({ role: user.role });
-      } else {
-        return response.status(404).json({ message: "No user found" });
+  const token = await getToken({ req: request });
+  if (token) {
+    console.log(token);
+    await dbConnect();
+    const { userEmail } = request.query;
+    if (request.method === "GET") {
+      try {
+        const user = await User.findOne({ email: userEmail });
+        if (user) {
+          return response.status(200).json({ role: user.role });
+        } else {
+          return response.status(404).json({ message: "No user found" });
+        }
+      } catch (error) {
+        return response.status(500).json({ message: "Server error" });
       }
-    } catch (error) {
-      return response.status(500).json({ message: "Server error" });
     }
+    return response.status(405).json({ status: "Method not allowed" });
+  } else {
+    return response
+      .status(401)
+      .json({ message: "Unauthorized access. Please log in." });
   }
-  return response.status(405).json({ status: "Method not allowed" });
 }
