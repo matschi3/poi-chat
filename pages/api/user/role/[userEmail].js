66fe0eadf479b2c9f384657182f3d29d5bc84e93
Nils Fischer <nils@designatic.de>
2024-03-19 18:34:45 +0100

    add check for users Email to protect api route from wrong-logged in user

--- pages/api/user/role/[userEmail].js
+++ pages/api/user/role/[userEmail].js
@@ -6,22 +6,27 @@ import { getToken } from "next-auth/jwt";
 export default async function handler(request, response) {
   const token = await getToken({ req: request });
   if (token) {
-    console.log(token);
-    await dbConnect();
     const { userEmail } = request.query;
-    if (request.method === "GET") {
-      try {
-        const user = await User.findOne({ email: userEmail });
-        if (user) {
-          return response.status(200).json({ role: user.role });
-        } else {
-          return response.status(404).json({ message: "No user found" });
+    if (token.email === userEmail) {
+      await dbConnect();
+      if (request.method === "GET") {
+        try {
+          const user = await User.findOne({ email: userEmail });
+          if (user) {
+            return response.status(200).json({ role: user.role });
+          } else {
+            return response.status(404).json({ message: "No user found" });
+          }
+        } catch (error) {
+          return response.status(500).json({ message: "Server error" });
         }
-      } catch (error) {
-        return response.status(500).json({ message: "Server error" });
       }
+      return response.status(405).json({ status: "Method not allowed" });
+    } else {
+      return response
+        .status(401)
+        .json({ message: "Unauthorized access. Wrong account." });
     }
-    return response.status(405).json({ status: "Method not allowed" });
   } else {
     return response
       .status(401)
