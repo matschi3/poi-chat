2202d0caff52cf1438c2ac0a7221f5c890a66cb5
Nils Fischer <nils@designatic.de>
2024-05-01 14:57:15 +0200

    secure api against not logged in members

--- pages/api/locations/index.js
+++ pages/api/locations/index.js
@@ -1,28 +1,36 @@
 import dbConnect from "../../../db/connect.js";
 import Location from "../../../db/models/Location.js";
+import { getToken } from "next-auth/jwt";
 
 export default async function handler(request, response) {
-  await dbConnect();
-  if (request.method === "GET") {
-    const locations = await Location.find({})
-      .populate({
-        path: "geometry",
-        populate: { path: "coordinates" },
-      })
-      .populate("adress");
-    if (!locations) {
-      return response.status(404).json({ message: "No locations found" });
+  const token = await getToken({ req: request });
+  if (token) {
+    await dbConnect();
+    if (request.method === "GET") {
+      const locations = await Location.find({})
+        .populate({
+          path: "geometry",
+          populate: { path: "coordinates" },
+        })
+        .populate("adress");
+      if (!locations) {
+        return response.status(404).json({ message: "No locations found" });
+      }
+      return response.status(200).json(locations);
     }
-    return response.status(200).json(locations);
-  }
-  if (request.method === "POST") {
-    try {
-      const newLocation = request.body;
-      await Location.create(newLocation);
-      return response.status(201).json({ status: "Location created." });
-    } catch (error) {
-      response.status(400).json({ error: error.message });
+    if (request.method === "POST") {
+      try {
+        const newLocation = request.body;
+        await Location.create(newLocation);
+        return response.status(201).json({ status: "Location created." });
+      } catch (error) {
+        response.status(400).json({ error: error.message });
+      }
     }
+    return response.status(405).json({ status: "Method not allowed" });
+  } else {
+    return response
+      .status(401)
+      .json({ message: "Unauthorized access. Please log in." });
   }
-  return response.status(405).json({ status: "Method not allowed" });
 }
