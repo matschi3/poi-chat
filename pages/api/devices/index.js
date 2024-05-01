cba29d9adbed0b05cd5e4b039da627b27255a767
Nils Fischer <nils@designatic.de>
2024-05-01 15:02:26 +0200

    secure api against not logged in members

--- pages/api/devices/index.js
+++ pages/api/devices/index.js
@@ -1,28 +1,36 @@
 import dbConnect from "../../../db/connect.js";
 import Device from "../../../db/models/Device.js";
+import { getToken } from "next-auth/jwt";
 
 export default async function handler(request, response) {
-  await dbConnect();
-  if (request.method === "GET") {
-    const devices = await Device.find({})
-      .populate("assets")
-      .populate({
-        path: "assets",
-        populate: { path: "images" },
-      });
-    if (!devices) {
-      return response.status(404).json({ message: "No devices found" });
+  const token = await getToken({ req: request });
+  if (token) {
+    await dbConnect();
+    if (request.method === "GET") {
+      const devices = await Device.find({})
+        .populate("assets")
+        .populate({
+          path: "assets",
+          populate: { path: "images" },
+        });
+      if (!devices) {
+        return response.status(404).json({ message: "No devices found" });
+      }
+      return response.status(200).json(devices);
     }
-    return response.status(200).json(devices);
-  }
-  if (request.method === "POST") {
-    try {
-      const newDevice = request.body;
-      await Device.create(newDevice);
-      return response.status(201).json({ status: "Device created." });
-    } catch (error) {
-      response.status(400).json({ error: error.message });
+    if (request.method === "POST") {
+      try {
+        const newDevice = request.body;
+        await Device.create(newDevice);
+        return response.status(201).json({ status: "Device created." });
+      } catch (error) {
+        response.status(400).json({ error: error.message });
+      }
     }
+    return response.status(405).json({ status: "Method not allowed" });
+  } else {
+    return response
+      .status(401)
+      .json({ message: "Unauthorized access. Please log in." });
   }
-  return response.status(405).json({ status: "Method not allowed" });
 }
