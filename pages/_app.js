9c10abe3fcca38ece7828dd0e30e39499dbd9154
Nils Fischer <nils@designatic.de>
2024-05-01 14:20:54 +0200

    add fetcher for useSWR and configure

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import GlobalStyle from "../styles.js";
 import { SessionProvider } from "next-auth/react";
+import { SWRConfig } from "swr";
+
+const fetcher = async (url) => {
+  try {
+    if (typeof url !== "string" || url.trim().length === 0) {
+      throw new Error("Invalid URL");
+    }
+
+    const response = await fetch(url);
+
+    if (!response.ok || response.status === 0) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("An error occurred while fetching data:", error);
+    throw new Error("An error occurred while fetching data: " + error.message);
+  }
+};
 
 export default function App({
   Component,
@@ -8,7 +28,9 @@ export default function App({
   return (
     <SessionProvider session={session}>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </SessionProvider>
   );
 }
