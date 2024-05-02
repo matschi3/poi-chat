ed1739e1baf6452b7946cc21ae353698b571eb91
Nils Fischer <nils@designatic.de>
2024-05-02 12:58:00 +0200

    create styled components Map container to keep styling same-same

---
+++ components/Map/Map.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledMap = styled.div`
+  width: 100%;
+  height: 30rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
