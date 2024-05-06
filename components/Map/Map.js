796d7b439b27b06a49acc672f81024d41de40924
Nils Fischer <nils@designatic.de>
2024-05-06 18:59:52 +0200

    recieve markers object, import Marker and Popup from Leaflet and render leaflets markers on given markers data

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,20 +1,25 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { StyledMap } from "./Map.styled";
 
-export default function Map() {
+export default function Map({ markers }) {
   return (
     <StyledMap>
       <MapContainer
         className="mymap"
-        center={[51.505, -0.09]}
+        center={[50.74238760309213, 7.103948755048992]}
         zoom={13}
-        scrollWheelZoom={true}
+        scrollWheelZoom={false}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         ></TileLayer>
+        {markers.map((marker) => (
+          <Marker key={marker.id} position={[marker.lat, marker.lng]}>
+            <Popup>{marker.name}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </StyledMap>
   );
