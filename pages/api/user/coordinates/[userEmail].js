a868cd746396e933c7802956636e3a78af026478
Nils Fischer <nils@designatic.de>
2024-05-07 09:48:09 +0200

    add api route to get users lastCoordinates

---
+++ pages/api/user/coordinates/[userEmail].js
@@ -0,0 +1,37 @@
+import dbConnect from "@/db/connect";
+import User from "@/db/models/User";
+import { getToken } from "next-auth/jwt";
+
+/* to get users lastCoordinates */
+export default async function handler(request, response) {
+  const token = await getToken({ req: request });
+  if (token) {
+    const { userEmail } = request.query;
+    if (token.email === userEmail) {
+      await dbConnect();
+      if (request.method === "GET") {
+        try {
+          const user = await User.findOne({ email: userEmail });
+          if (user) {
+            return response
+              .status(200)
+              .json({ lastCoordinates: user.lastCoordinates });
+          } else {
+            return response.status(404).json({ message: "No user found" });
+          }
+        } catch (error) {
+          return response.status(500).json({ message: "Server error" });
+        }
+      }
+      return response.status(405).json({ status: "Method not allowed" });
+    } else {
+      return response
+        .status(401)
+        .json({ message: "Unauthorized access. Wrong account." });
+    }
+  } else {
+    return response
+      .status(401)
+      .json({ message: "Unauthorized access. Please log in." });
+  }
+}
